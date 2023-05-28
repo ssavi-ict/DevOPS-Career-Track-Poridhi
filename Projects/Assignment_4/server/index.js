e34caef614d6e2d6eb74f84f01f9d5bc2c5cac4b
ssavi.ict <ssavi.ict@gmail.com>
2023-05-29 01:27:48 +0600

    latest changes

--- Projects/Assignment_4/server/index.js
+++ Projects/Assignment_4/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const app = express();
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 const db = require("./models");
 
-// Routers
+// Routers Are
 const postRouter = require('./routes/Posts');
 app.use("/posts", postRouter);
 
