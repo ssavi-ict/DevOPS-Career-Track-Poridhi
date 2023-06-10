5030db6222a14d2738f91bcda084fce1ed8aacc6
ssavi.ict <ssavi.ict@gmail.com>
2023-06-10 12:03:19 +0600

    exam assignment

---
+++ Projects/Assignment_5/client/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main>
+      <p className="text-center my-5 text-lg">
+        Hello and Welcome.
+      </p>
+      
+      <p className="text-center font-bold my-5 text-lg">
+        Avik Sarkar
+      </p>
+      <img src="/vite.svg" alt="poridhi-logo" className="mx-auto w-96" />
+    </main>
+  );
+};
+
+export default App;
